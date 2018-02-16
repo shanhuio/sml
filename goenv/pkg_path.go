db5116b2374825c4cad1eed2776fe14152b3aab9
Lonnie Liu <liulonnie@gmail.com>
2018-02-15 23:48:25 -0800

    add support for mapping files

--- goenv/pkg_path.go
+++ goenv/pkg_path.go
@@ -4,13 +4,22 @@ import (
 	"path/filepath"
 )
 
+func pkgPath(subDir, p string) string {
+	return filepath.Join(subDir, filepath.FromSlash(p))
+}
+
 // PkgPath returns the file path of a sub directory (e.g. src, bin, etc.)
 // for a particular package.
 func PkgPath(subDir, pkg string) string {
-	return filepath.Join(subDir, filepath.FromSlash(pkg))
+	return pkgPath(subDir, pkg)
 }
 
 // SrcDir returns the Go language source directory.
 func SrcDir(pkg string) string {
 	return PkgPath("src", pkg)
+}
+
+// SrcFile returns the file path of a Go language source file.
+func SrcFile(f string) string {
+	return pkgPath("src", f)
 }
