4f1b35d7dc9dcf4237adfe15a6e5044bbf268657
Lonnie Liu <liulonnie@gmail.com>
2018-03-30 23:20:38 -0700

    add repo mirroring

--- core/build_config.go
+++ core/build_config.go
@@ -4,8 +4,11 @@ package core
 type BuildConfig struct {
 	// TestDataWhiteList specifies testdata directories that we will still
 	// treat as Go language packages rather than data.
-	TestDataWhiteList []string
+	TestDataWhiteList []string `json:",omitempty"`
 
 	// PkgBlackList specifies directories that we are not building.
-	PkgBlackList []string
+	PkgBlackList []string `json:",omitempty"`
+
+	// RepoFixes specifies repo mirrors required to make things build.
+	RepoFixes map[string]string `json:",omitempty"`
 }
