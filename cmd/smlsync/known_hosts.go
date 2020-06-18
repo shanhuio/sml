8325b03d53a960e5ff7a584075531b75cb5b4a59
Lonnie Liu <liulonnie@gmail.com>
2020-06-17 18:42:24 -0700

    add github known hosts signature

--- cmd/smlsync/known_hosts.go
+++ cmd/smlsync/known_hosts.go
@@ -7,4 +7,12 @@ const knownHostsContent = `bitbucket.org ssh-rsa ` +
 	`WMXNMdFP3jHAJH0eDsoiGnLPBlBp4TNm6rYI74nMzgz3B9IikW4WVK+dc8KZJ` +
 	`ZWYjAuORU3jc1c/NPskD2ASinf8v3xnfXeukU0sJ5N6m5E8VLjObPEO+mN2t/` +
 	`FZTMZLiFqPWc/ALSqnMnnhwrNi2rbfg/rd/IpL8Le3pSBne8+seeFVBoGqzHM` +
-	`9yXw==` + "\n"
+	`9yXw==` + "\n" +
+	`github.com ssh-rsa ` +
+	`AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXY` +
+	`PCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJ` +
+	`NlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84Kez` +
+	`mD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqq` +
+	`UUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGH` +
+	`SZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUF` +
+	`FAaQ==` + "\n"
